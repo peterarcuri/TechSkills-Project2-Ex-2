5295de35e48ba5c2280abf4ba98800adf4d1dacf
Petersix <150849737+peterarcuri@users.noreply.github.com>
2024-07-24 02:45:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
 
-// ---------------------------------------------------------------------------
-// // QUESTION 1(potential variables needed for Julius Caesar's secret party):
-// ---------------------------------------------------------------------------
-partyLocation
-shiftValue
-encrypt
-decode
-isPartySafe
-
-// ----------------------------------------------------------------------
-// // QUESTION 2(specify primitive data type & provide a value for each):
-// ----------------------------------------------------------------------
-let partyLocation = "garden"; // string
-const shiftValue = 3; // (number)
-let encrypt = "sdafsad"; // (string)
-let decode = "garden"; // (string)
-let isPartySafe = true; // (boolean)
-
-// ----------------------------------------------------------------------------------------------------------------
-// // QUESTION 3(determine which variables should be declared as "const" or which ones should be declared as "let"):
-// ----------------------------------------------------------------------------------------------------------------
-
-// (party location could change)
-let partyLocation = "garden"  
-
-// (only variable that remains constant. Always shifting the value by 3)
-const shiftValue = 3; 
-
-// The encrypted message could change to any location
-let encrypt = "sdafsad"; (string)
-
-// The decoded message could change as well
-let decode = "garden"; (string)
-
-// The partys' safety could be either true or false (not constant)
-let isPartySafe = true; (boolean)
-
-// --------------------------------------------------------------------------------------------------------
-// // QUESTION 4(Given the variable shiftValue, write a piece of code to check if its value is an integer):
-// --------------------------------------------------------------------------------------------------------
+const friend = "BRUTUS";
 const shiftValue = 3;
-Number.isInteger(shiftValue);
+
+// ================
+// ==== STEP 1 ====
+// ================
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// ================
+// ==== STEP 2 ====
+// ================
+
+const firstLetter = friend[0];
+const index = alphabet.indexOf(firstLetter.toLowerCase());
+
+
+// ====================
+// ==== QUESTION 1 ====
+// ====================
+
+// ANSWER: The reason it is 1 instead of 2 is because the index of a string always starts with 0.
+
+// ================
+// ==== STEP 3 ====
+// ================
+
+const newIndex = index + shiftValue;
+const encryptedFirstLetter = alphabet[newIndex];
+
+// ====================
+// ==== QUESTION 2 ====
+// ====================
+
+// ANSWER: The modulus operator is what can help us wrap around and continue from the beginning of the alphabet
+
+// ================
+// ==== STEP 4 ====
+// ================
+
+const alphabetLength = alphabet.length;
+
+// ================
+// ==== STEP 5 ====
+// ================
+
+const alphabetLength = alphabet.length;
+const newIndex = (index + shiftValue) % alphabetLength;
+const encryptedFirstLetter = alphabet[newIndex];
+
+// ================
+// ==== STEP 6 ====
+// ================
+
+const encryptedMessage = "EUXWXV";
+const teaserMessage = encryptedMessage.slice(0, 3);
